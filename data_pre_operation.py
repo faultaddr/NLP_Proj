4526778ffc8fe303ea74cfd081a06cd039fa6c50
panyunyi97 <779087031@qq.com>
2018-10-15 20:53:04 +0800

    initial function complete
    N/A
    
    initial function complete
    
    Signed-off-by: panyunyi97 <779087031@qq.com>

---
+++ data_pre_operation.py
@@ -0,0 +1,11 @@
+def get_dict():
+    fp = open("./data/data.txt", 'r', encoding="utf-8")
+    str_list = []
+    for line in fp.readlines():
+        str_list.append(line.split("\n")[0].split("  ")[:-1])
+    str_dic = {}
+    fp.close()
+    for s in str_list:
+        key = s[0]
+        str_dic[key] = s[1:]
+    return str_dic
---
+++ input_opt.py
@@ -0,0 +1,75 @@
+from __future__ import print_function
+from data_pre_operation import get_dict
+
+str_dict = get_dict()
+print("---------------****** 英语字典 ******---------------")
+print("本软件最终解释权归种菜的小朋友所有 @copyright")
+print("please input what you what to know:  ")
+input_str = input()
+temp_str = input_str.split()
+
+
+def handle_error():
+    print("------****** 本单词暂时还未被潘氏大辞典收录 ******------")
+
+
+def opt_rules(raw_str: str):
+    """
+
+    :param raw_str:
+    :return:
+    """
+    temp1 = temp2 = temp3 = temp4 = ""
+    if raw_str.endswith("s"):
+        if raw_str[-3:-1] == 'ie':
+            temp3 = raw_str[:-3] + "y"
+        if raw_str[-2] == 'e':
+            temp2 = raw_str[:-2]
+        temp1 = raw_str[:-1]
+
+    elif raw_str.endswith("ed"):
+        if raw_str[-3] == 'i':
+            temp3 = raw_str[:-3] + "y"
+        if len(raw_str) >= 4 and raw_str[-4] == raw_str[-3]:
+            temp4 = raw_str[:-3]
+        temp1 = raw_str[:-2]
+        temp2 = raw_str[:-1]
+
+    elif raw_str.endswith("ing"):
+        if raw_str[-4] == 'y':
+            temp3 = raw_str[:-5] + 'ie'
+        if len(raw_str) >= 5 and raw_str[-5] == raw_str[-4]:
+            temp4 = raw_str[:-4]
+        temp2 = raw_str[:-3]
+        temp1 = raw_str[:-3] + 'e'
+    else:
+        temp = []
+        fp = open("./data/v.txt", 'r')
+        for line in fp.readlines():
+            if raw_str in line.split():
+                temp.append(line[0])
+        return temp
+    return [temp1, temp2, temp3, temp4]
+
+
+for i in temp_str:
+    if str_dict.get(i) is not None:
+        print(i, "  查询结果如下所示：----****----")
+        for j in range(len(str_dict[i])):
+            if j % 2 == 0:
+                print("  ", str_dict[i][j])
+            else:
+                print("    ", str_dict[i][j])
+    else:
+        opted = opt_rules(i)
+        for x in opted:
+            if x != "":
+                if str_dict.get(x) is not None:
+                    print(x, "  查询结果如下所示：----****----")
+                    for j in range(len(str_dict[x])):
+                        if j % 2 == 0:
+                            print("  ", str_dict[x][j])
+                        else:
+                            print("    ", str_dict[x][j])
+            else:
+                pass
